881c91c3ce654a88705c4646be631a47bb5e7a60
Himanshu Khandelwal <himanshu@Himanshus-MacBook-Air.local>
2026-06-25 18:53:10 +0530

    changes

---
+++ test3.py
@@ -0,0 +1,7 @@
+from paddleocr import PaddleOCRVL
+pipeline = PaddleOCRVL(pipeline_version="v1")
+output = pipeline.predict("https://paddle-model-ecology.bj.bcebos.com/paddlex/imgs/demo_image/paddleocr_vl_demo.png")
+for res in output:
+    res.print()
+    res.save_to_json(save_path="output")
+    res.save_to_markdown(save_path="output")
